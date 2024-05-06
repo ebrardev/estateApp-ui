78e1325c6b0d9118b7f80cff974a3440e3c32be0
ebrardev <16155052@mersin.edu.tr>
2024-05-06 13:36:27 +0300

    Refactor App component to include Navbar and PageHome components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./components/Navbar/Navbar"
 import "./layout.scss"
+import PageHome from "./routes/PageHome/pageHome"
+
 
 function App() {
   return (
     <div className="layout"> 
+    <div className="navbar">
     <Navbar />
-    
+    </div>
+   <div className="content">
+    <PageHome />
+   </div>
+
     </div>
   )
 }
---
+++ src/routes/PageHome/pageHome.jsx
@@ -0,0 +1,10 @@
+import "./pageHome.scss";
+
+const PageHome= () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+export default PageHome;
