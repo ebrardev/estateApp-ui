1ffffecc5e1a90be34be30cdb707f6c93b1e32ff
ebrardev <16155052@mersin.edu.tr>
2024-05-06 12:43:26 +0300

    hamburger design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
-
+import React, { useState } from "react";
 import "./navbar.scss"
 function Navbar() {
+
+    const [open, setOpen] = useState(false)
     return (
         <nav>
             <div className="left">
@@ -16,6 +18,22 @@ function Navbar() {
             <div className="right">
                 <a href="/">Login</a>
                 <a href="/" className="register">Sign Up</a>
+
+                <div className="menuIcon">
+                    <img src="/menu.png" 
+                    alt="menu" 
+                    onClick={() => setOpen(prev=>!prev)}
+                    />
+
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+                <a href="/">Login</a>
+                <a href="/" className="register">Sign Up</a>
+                </div>
             </div>
         </nav>
     )
