277ef7192e3e769c9943cb9f0844cb85e3e547ce
ebrardev <16155052@mersin.edu.tr>
2024-05-06 23:16:00 +0300

    Update search bar component and styles

--- src/routes/PageHome/pageHome.jsx
+++ src/routes/PageHome/pageHome.jsx
@@ -1,5 +1,5 @@
 
-import SearchBar from "../../components/searchBar/Searchbar";
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./pageHome.scss";
 
 const PageHome= () => {
@@ -14,6 +14,20 @@ const PageHome= () => {
 
 </p>
 <SearchBar />
+<div className="boxes">
+    <div className="box">
+        <h1>16+</h1>
+        <h2>Years of Experience</h2>
+    </div>
+    <div className="box">
+        <h1>290</h1>
+        <h2>Award Gained</h2>
+    </div>
+    <div className="box">
+        <h1>12000</h1>
+        <h2>Property Ready</h2>
+    </div>
+</div>
 </div>
      </div>
         <div className="imageContainer">
