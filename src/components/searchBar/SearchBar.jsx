446f11f298d0c979358b0b3b3378370ceee5349d
ebrardev <16155052@mersin.edu.tr>
2024-05-07 00:48:38 +0300

    homepage style

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -32,7 +32,7 @@ function SearchBar() {
             <input type="number " name="minPrice" min={0} max={100000000} placeholder="Min Price" />
             <input type="number " name="maxPrice" min={0} max={100000000} placeholder="Max Price" />
         </form>
-        <button>
+        <button >
             <img src="/search.png" alt="search" />
         </button>
         </div>
